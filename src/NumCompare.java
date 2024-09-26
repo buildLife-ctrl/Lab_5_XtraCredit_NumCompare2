8de62b1536351738caa5989711d0dafbe6cbe946
617998 <sinchuri.617998@swl.k12.oh.us>
2024-09-26 09:39:53 -0400

    2

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,5 +1,33 @@
-public class Main {
+import java.util.Scanner;
+
+public class NumCompare {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        //variable
+        int firstInteger;
+        int secondInteger;
+
+        //asks the user for integers to compare
+        System.out.println("Enter your first integer:");
+        if (scan.hasNextInt()) {
+            firstInteger = scan.nextInt();
+            System.out.println("Enter your second integer:");
+            if (scan.hasNextInt()) {
+                secondInteger = scan.nextInt();
+                if (firstInteger > secondInteger) {
+                    System.out.println(firstInteger + " is greater than " + secondInteger);
+                } else if (firstInteger < secondInteger) {
+                    System.out.println(firstInteger + " is less than " + secondInteger);
+                } else {
+                    System.out.println(firstInteger + " is equal to " + secondInteger);
+                }
+            } else {
+                System.out.println("You have entered an invalid date type.");
+                System.exit(0);
+            }
+        } else {
+            System.out.println("You have entered an invalid date type.");
+            System.exit(0);
+        }
     }
 }
